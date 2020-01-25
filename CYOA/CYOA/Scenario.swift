28906eb6074e4f995eb88c16c0a89c1ff4982ab8
Michael <55179472+dondestefano@users.noreply.github.com>
2020-01-25 14:37:21 +0100

    Basics in place
    The app can provide two answers and change its text depenfing on which answer the user provides.

---
+++ CYOA/CYOA/Scenario.swift
@@ -0,0 +1,47 @@
+//
+//  Scenario.swift
+//  CYOA
+//
+//  Created by Luigi Anonymus on 2020-01-25.
+//  Copyright © 2020 Michael De Stefano. All rights reserved.
+//
+
+import Foundation
+class Scenario {
+    
+    var act : Int?
+    var chapter: Int?
+    var paragraphOne : String?
+    var paragraphTwo : String?
+    var paragraphThree : String?
+    var optionOne : String?
+    var optionTwo : String?
+    
+    init (act: Int, chapter: Int) {
+        self.act = act
+        self.chapter = chapter
+    }
+    
+    func nextScenario (act: Int, chapter: Int, paragraphOne: String?, paragraphTwo: String?, paragraphThree: String?, optionOne: String?, optionTwo: String?) {
+        self.act = act
+        self.chapter = chapter
+        self.paragraphOne = paragraphOne
+        self.paragraphTwo = paragraphTwo
+        self.paragraphThree = paragraphThree
+        self.optionOne = optionOne
+        self.optionTwo = optionTwo
+    }
+    
+    func getParagraph() -> String {
+        var paragraph = ""
+        if let paragraphOne = paragraphOne, let paragraphTwo = paragraphTwo, let paragraphThree = paragraphThree {
+            paragraph = "\(paragraphOne) \(paragraphTwo) \(paragraphThree)"
+        } else if let paragraphOne = paragraphOne, let paragraphThree = paragraphThree {
+            paragraph = "\(paragraphOne) \(paragraphThree)"
+        } else if let paragraphOne = paragraphOne, let paragraphTwo = paragraphTwo {
+            paragraph = "\(paragraphOne) \(paragraphTwo)"
+        }
+        return paragraph
+    }
+    
+}
--- CYOA/CYOA/ViewController.swift
+++ CYOA/CYOA/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentScenario = Scenario (act: 0, chapter: 0)
+    
+    @IBOutlet weak var storyText: UITextView!
+    
+    @IBOutlet weak var optionOneButton: UIButton!
+    
+    @IBOutlet weak var optionTwoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentScenario.nextScenario(act: 1, chapter: 1, paragraphOne: "Hej", paragraphTwo: "Vad", paragraphThree: "Händer", optionOne: "Inget", optionTwo: "massor")
+        
+        updateStory()
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressedButtonOne(_ sender: Any) {
+        currentScenario.nextScenario(act: 1, chapter: 2, paragraphOne: "Låter", paragraphTwo: "Mega", paragraphThree: "Trist", optionOne: "K", optionTwo: "Bry")
+        updateStory()
+        
+    }
+    
+    @IBAction func pressedButtonTwo(_ sender: Any) {
+            currentScenario.nextScenario(act: 1, chapter: 2, paragraphOne: "Lol.", paragraphTwo: "Berätta", paragraphThree: "Mer", optionOne: "Nej", optionTwo: "Ok")
+        updateStory()
+    }
+    
+    func updateStory() {
+        let currentParagraph = currentScenario.getParagraph()
+        
+        storyText.text = currentParagraph
+        optionOneButton.setTitle("\(currentScenario.optionOne ?? "")", for: .normal)
+        optionTwoButton.setTitle("\(currentScenario.optionTwo ?? "")", for: .normal)
+    }
+    
 }
-
